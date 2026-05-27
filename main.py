428b3c5fe91e02e65ef0b6ef8abd92270b66c669
Tangu Van Hullebusch <tanguyvh1@gmail.com>
2026-05-27 12:31:59 +0200

    Update Main.py

--- main.py
+++ main.py
@@ -1,13 +1,67 @@
-from fastapi import FastAPI
+import network # type: ignore
+import time
+from machine import Pin # type: ignore
+import dht # type: ignore
+import ujson # type: ignore
+from umqtt.simple import MQTTClient  # type: ignore
 
-app = FastAPI()
+"""
+
+BASED ON MicroPython ESP32 DHT22 MQTT
+by artefatos
+https://wokwi.com/projects/395442253921303553
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+
+USE WOKWI HOBBY + 
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: str | None = None):
-    return {"item_id": item_id, "q": q}
+"""
+
+
+# MQTT Server Parameters
+MQTT_CLIENT_ID = " "
+MQTT_BROKER    = "host.wokwi.internal" 
+MQTT_PORT = 1883
+MQTT_USER      = " "
+MQTT_PASSWORD  = " "
+MQTT_TOPIC     = " "
+
+sensor = dht.DHT22(Pin(15))
+
+print("Connecting to WiFi", end="")
+sta_if = network.WLAN(network.STA_IF)
+sta_if.active(True)
+sta_if.connect('Wokwi-GUEST', '')
+while not sta_if.isconnected():
+  print(".", end="")
+  time.sleep(0.1)
+print(" Connected!")
+
+print("Connecting to MQTT server... ", end="")
+client = MQTTClient(MQTT_CLIENT_ID, MQTT_BROKER, port=MQTT_PORT, user=MQTT_USER, password=MQTT_PASSWORD)
+client.connect()
+
+print("Connected!")
+
+
+while True:
+  print("Measuring weather conditions... ", end="")
+  try:
+    sensor.measure() 
+    message = ujson.dumps({
+      "temp": sensor.temperature(),
+      "humidity": sensor.humidity(),
+    })
+  except Exception as e:
+    print("Erreur :", e)
+    
+  time.sleep(30)
+  print("Updated!")
+  print("Reporting to MQTT topic {}: {}".format(MQTT_TOPIC, message))
+  client.publish(MQTT_TOPIC, message)
+    
+  
+ 
+
+
