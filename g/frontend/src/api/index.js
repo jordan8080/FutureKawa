e3205e4931b75bcc918780a5bdb879a23d24febb
lamin-mbarek <laminmbarek2022@gmail.com>
2026-06-19 11:44:25 +0200

    correction Merge backend, frontend et ajout Node-RED pour l'alerting

--- g/frontend/src/api/index.js
+++
@@ -1,5 +0,0 @@
-export { lotsApi } from './lotsApi'
-export { sensorsApi } from './sensorsApi'
-export { alertsApi } from './alertsApi'
-export { warehousesApi } from './warehousesApi'
-export { countriesApi } from './countriesApi'
